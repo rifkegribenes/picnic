76c4b4aa3b25df898a4072d48efff695bd154ff5
Sarah Schneider <none>
2018-06-23 21:53:02 +0000

    fix merge commit

--- client/src/store/actions/apiConfig.js
+++ client/src/store/actions/apiConfig.js
@@ -5,25 +5,17 @@
 
 /* ================================= SETUP ================================= */
 
-<<<<<<< HEAD
+
 const prodUrl = 'https://picnic.glitch.me';
 // const devUrl = "http://localhost:3001"; // server url for local install
-=======
-// const prodUrl = 'https://picnic.glitch.me';
-const devUrl = "http://localhost:3001"; // server url for local install
->>>>>>> 6b524d4a91df10a32d71223cf1eba36a058f96e4
 
 /* ================================ EXPORTS ================================ */
 
 // ENVIRONMENT is a global variable defined by weback.config.js
 // defaults to DEVELOPMENT
 
-<<<<<<< HEAD
 // export const BASE_URL = devUrl;
 export const BASE_URL = prodUrl;
-=======
-export const BASE_URL = devUrl;
-// export const BASE_URL = prodUrl;
->>>>>>> 6b524d4a91df10a32d71223cf1eba36a058f96e4
+
 
 // export const BASE_URL = (ENVIRONMENT === 'PRODUCTION' ? prodUrl : devUrl);
